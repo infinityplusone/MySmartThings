94e219eab286504ef5a0574ed9ed93a3ba611f61
infinityplusone <jonathan@infinityplusone.com>
2017-12-28 12:07:17 -0500

    fuck

--- smartapps/infinityplusone/water-pump-sensor/water-pump-sensor.groovy
+++ smartapps/infinityplusone/water-pump-sensor/water-pump-sensor.groovy
@@ -1,10 +1,10 @@
 /**
- *  Water Pump Sensor
+ *  ZP Pet Water Fountain
  *
- *  Author: Jonathan Knoll
+ *  Author: ZPriddy
  */
 definition(
-    name: "Water Pump Sensor",
+    name: "ZP Pet Water Fountain",
     namespace: "infinityplusone",
     author: "infinityplusone",
     description: "Monitors a contact switch that is installed on a Litter Robot and when the selected number of cycles compleate it will send a notification",
@@ -14,39 +14,40 @@ definition(
 )
 
 preferences {
-  section("Water Pump Sensor") {
+  section("Pet Water Fountain Sensor") {
     input "waterSensor", "capability.waterSensor"
   }
 
-  section("Water Pump Sensor Virtual Device"){
-    input "waterPump", "capability.waterSensor", title: "Water Pump Sensor Virtual Device"
+  section("Pet Water Fountain Virtual Device"){
+    input "waterFountain", "capability.waterSensor", title: "Pet Water Fountain Virtual Device"
   }
-
-  section("Notify Every X Minutes") {
-    input "minutesDelay", "number", title: "Minute Delay"
+  section("Notify Every X Hours") {
+    input "hoursDelay", "number", title: "Hour Delay"
   }
-    // section("Auto Shutoff Options"){
-    //   input "waterSwitch","capability.switch",title:"Water Pump Sensor Switch", required:false
-    // }
+    section("Auto Shutoff Options"){
+      input "waterSwitch","capability.switch",title:"Pet Water Fountain Switch", required:false
+    }
   
-  section( "Notifications" ) {
-      input("recipients", "contact", title: "Send notifications to") {
-          input "sendPushMessage", "enum", title: "Send a push notification?", options: ["Yes", "No"], required: false
-          input "phone1", "phone", title: "Send a Text Message?", required: false
-      }
-  }
+    section( "Notifications" ) {
+        input("recipients", "contact", title: "Send notifications to") {
+            input "sendPushMessage", "enum", title: "Send a push notification?", options: ["Yes", "No"], required: false
+            input "phone1", "phone", title: "Send a Text Message?", required: false
+        }
+    }
   
 }
 
 def installed() {
   subscribe(waterSensor, "water", waterLevelHandler)
-  // subscribe(waterPump, "switch", waterSwitchHandler)
+    subscribe(waterSwitch, "switch", waterSwitchHandler)
+    subscribe(waterFountain, "switch", waterSwitchHandler)
 }
 
 def updated() {
   unsubscribe()
   subscribe(waterSensor, "water", waterLevelHandler)
-  // subscribe(waterPump, "switch", waterSwitchHandler)
+    subscribe(waterSwitch, "switch", waterSwitchHandler)
+    subscribe(waterFountain, "switch", waterSwitchHandler)
 
 }
 
@@ -54,23 +55,61 @@ def waterLevelHandler(evt) {
   if (evt.value == "dry")
     {
       log.debug "Water Sensor Dry"
-    runIn(3 * 60, notifyDisabled);
+    runIn(3 * 60, notifyEmpty);
     }
     if (evt.value == "wet")
     {
-      waterPump.wet()
+      waterFountain.wet()
       log.debug "Water Sensor Wet"
-      unschedule(notifyDisabled)
-      waterPump.on()
+      unschedule(notifyEmpty)
+        waterFountain.on()
+    }
+
+}
+
+
+def notifyEmpty() {
+  send("Pet Water Fountain is EMPTY! Check on it soon!")
+    runIn(hoursDelay * 60 * 60, notifyEmpty);
+    waterFountain.dry()
+    waterFountain.off()
+    waterSwitch.off()
+}
+
+def waterSwitchHandler(evt) {
+  log.debug "Switch Handler" 
+  def waterLevel = waterSensor.currentValue("water")
+    log.debug waterLevel
+    log.debug evt.value
+    if (evt.value == "on")
+    {
+        if (waterLevel == "dry")
+        {
+      send("Pet Water Fountain is still EMPTY! It cant be turned on!")
+            waterFountain.off()
+            waterSwitch.off()
+
+        }
+        else
+        {
+            waterFountain.on()
+            waterSwitch.on()
+        }
+    //Other case is not needed because it would be done in waterLevel Handler
+    }
+    if (evt.value == "off")
+    {
+        waterFountain.off()
+
+        waterSwitch.off()
+
     }
 }
 
-def notifyDisabled() {
-  send("Water Pump is disabled! Check on it soon!")
-    runIn(minutesDelay * 60, notifyDisabled);
-    waterPump.ok()
-    waterPump.disabled()
-}
+
+
+
+
 
 private send(msg) {
     if (location.contactBookEnabled) {
--- smartapps/infinityplusone/water-pump-sensor/water-pump-sensor_Device.groovy
+++ smartapps/infinityplusone/water-pump-sensor/water-pump-sensor_Device.groovy
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2017 Jonathan Knoll
+ *  Copyright 2015 Zachary Priddy
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -12,13 +12,14 @@
  *
  */
 metadata {
-  definition (name: "Water Pump Sensor", namespace: "infinityplusone", author: "infinityplusone") {
+  definition (name: "ZP Pet Water Fountain", namespace: "infinityplusone", author: "infinityplusone") {
     capability "Indicator"
+    capability "Switch"
     capability "Sensor"
-    capability "Water Sensor"
+        capability "Water Sensor"
         
-    command "ok"
-    command "disabled"
+    command "wet"
+        command "dry"
 
   }
 
@@ -29,25 +30,42 @@ metadata {
   tiles(scale: 2) {
       multiAttributeTile(name:"water", type: "water", width: 6, height: 4){
       tileAttribute ("device.water", key: "PRIMARY_CONTROL") {
-        attributeState "ok", label:'OK', icon:"st.alarm.water.dry", backgroundColor:"#53a7c0"
-        attributeState "disabled", label:'Full', icon:"st.alarm.water.water", backgroundColor:"#ff0000"
+        attributeState "wet", label:'FULL', icon:"st.alarm.water.dry", backgroundColor:"#53a7c0"
+        attributeState "dry", label:'EMPTY', icon:"st.alarm.water.water", backgroundColor:"#ff0000"
       }
     }
-  }
+        
+        standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+            state "off", label: '${name}', action: "on", icon: "st.switches.switch.off", backgroundColor: "#ff0000"
+        state "on", label: '${name}', action: "off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+}
+
     main(["water"])
+    details(["water","switch"])
   }
 }
 
 def parse(String description) {
 }
 
-
-def ok() {
-  sendEvent(name: "water", value: "ok")
-    log.info "OK"
+def on() {
+  sendEvent(name: "switch", value: "on")
+    log.info "Water Fountain On"
 }
 
-def disabled() {
-  sendEvent(name: "water", value: "disabled")
-    log.info "Full"
+
+def off() {
+  sendEvent(name: "switch", value: "off")
+    log.info "Water Fountain Off"
+}
+
+
+def dry() {
+  sendEvent(name: "water", value: "dry")
+    log.info "Water Fountain Empty"
+}
+
+def wet() {
+  sendEvent(name: "water", value: "wet")
+    log.info "Water Fountain Full"
 }
